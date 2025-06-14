eecb59e761c5553110bec31d9a49897bf68b5f28
Khalid Himmo <khalidhimmo07@gmail.com>
2025-06-14 14:00:50 +0400

    Added platform generator tool (broken collision)

---
+++ Source/Test1/Private/APlatformGenerator.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:419d4334804b0aa1798dae3b80301b00ec494340532b1c0fa75f3d978b19591c
+size 9417
---
+++ Source/Test1/Public/APlatformGenerator.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c00ada7eb3aae66d86a7638253f5e7c36ef4dc9589cf4a8b96632504dc5ea188
+size 2766
