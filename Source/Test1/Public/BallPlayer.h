a010bddccbf2c6eb46c6648326fc1c3f29d4ee66
Khalid Himmo <khalidhimmo07@gmail.com>
2025-06-25 08:55:32 +0400

    Experimenting with movement (still unfinfished)

--- Source/Test1/Public/BallPlayer.h
+++ Source/Test1/Public/BallPlayer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e4573f7bd1e180dcff2290218f6347a06f4219bc1306c2483c91fbf7caafec6
-size 3968
+oid sha256:5cebb1e9d3dee2b18051243aff96a05729d5c3516921cd71679178e20ad0e5a7
+size 4082
