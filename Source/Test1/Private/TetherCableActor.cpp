ddc5c29aa8da31f995a71bc16daa9a7823b08f92
Khalid Himmo <khalidhimmo07@gmail.com>
2025-08-15 13:34:08 +0400

    polished slingshot mechanics

--- Source/Test1/Private/TetherCableActor.cpp
+++ Source/Test1/Private/TetherCableActor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94305b1f6cd91b3058ed3422e0dfa13ba0a59fd4ed3438d4a5f5cba2e857eeb4
-size 14232
+oid sha256:04e72572303fbd59044b5b577c864ab28f9adcfcde06535c95f423c97ef65f5e
+size 14310
